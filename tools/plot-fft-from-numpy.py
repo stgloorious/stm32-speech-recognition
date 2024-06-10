Fix spectrogram calculation
Fix high FFT value at 0 introduced by hanning window multiplication by
first removing the DC offset and then applying the window.

Signed-off-by: Stefan Gloor <code@stefan-gloor.ch>

tools/plot-fft-from-numpy.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 
 # Read the binary file and convert the data to an array of uint8
 with open('output.bin', 'rb') as f:
-    x = np.frombuffer(f.read(), dtype=np.int8)
+    x = np.frombuffer(f.read(), dtype=np.uint8)
 
-x = x[256*10:256*11]
+window_size = 256
+window_to_inspect = 10
+x = x[window_size * window_to_inspect : window_size * (window_to_inspect + 1)]
 x = x - np.mean(x)
-print(f'mean={np.mean(x)}')
+x = x * np.hanning(256)
 
 samplingrate = 16000
 
@@ -23,7 +25,7 @@ freq = n / T
 # Plot the FFT results with frequency in hertz
 plt.figure(figsize=(10, 6))
 plt.plot(freq[:N//2], X[:N//2])  # Plot only the positive frequencies
-plt.title('FFT of 440 Hz Test Signal Calculated Using numpy')
+plt.title('FFT of Test Signal Calculated Using numpy')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
 plt.grid(True)

tools/plot-fft-from-stm32.py
@@ -9,7 +9,8 @@ with open('data.txt', 'r') as f:
     for line in f:
         X.append(float(line.strip()))
 
-X = X[10*128:11*128]
+# Get the first FFT (N+1 points)
+X = X[129 * 20 : 129 * 20 + 129]
 samplingrate = 16000
 N = len(X)*2
 n = np.arange(N)
@@ -19,7 +20,7 @@ freq = n / T
 # Plot the FFT results with frequency in hertz
 plt.figure(figsize=(10, 6))
 plt.plot(freq[:N//2], X)  # Plot only the positive frequencies
-plt.title('FFT of 440 Hz Test Signal Calculated By STM32')
+plt.title('FFT of Test Signal Calculated By STM32')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
 plt.grid(True)

tools/spectrogram.py
@@ -11,7 +11,7 @@ window_size = 256
 frame_step = 128
 samplingrate = 16000
 
-norm = mcolors.Normalize(-10, 1.5)
+norm = mcolors.Normalize(-10, 2)
 
 # Apply STFT to waveforms to get spectrogram
 def get_spectrogram(waveform):
@@ -49,7 +49,8 @@ def get_numpy_spec(waveform):
     spectrogram_numpy = []
     for idx in range(math.ceil(len(waveform)/frame_step) - 1):
         signal_chunk = waveform[idx * frame_step : idx * frame_step + window_size]
-        signal_chunk = signal_chunk * np.hanning(window_size);
+        signal_chunk = signal_chunk - np.mean(signal_chunk)
+        signal_chunk = signal_chunk * np.hanning(window_size)
         X = np.abs(np.fft.fft(signal_chunk))
         spectrogram_numpy.append(np.array(X[:frame_step+1]))
 
@@ -107,6 +108,7 @@ with open('data.txt', 'r') as f:
 
 spectrogram_cmsis = []
 for i in range(124):
+    # The result of an N-point FFT is (N+1) points
     data_window = data[i * (frame_step + 1): i * (frame_step + 1) + window_size]
     spectrogram_cmsis.append(np.array(data_window[:frame_step+1]))
 spectrogram_cmsis = np.array(spectrogram_cmsis)